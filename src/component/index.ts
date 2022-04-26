6139dde1cd391ad1b680ad9ebada4fafb8beaf2b
Jtplouffe <jtplouffe@gmail.com>
2022-04-25 20:51:22 -0400

    Fix path

--- src/component/index.ts
+++ src/component/index.ts
@@ -3,54 +3,91 @@ import {
     apply,
     filter,
     mergeWith,
+    move,
     Rule,
     SchematicContext,
     template,
     Tree,
-    url,
+    url
 } from "@angular-devkit/schematics";
-import { Schema } from "./schema";
+
+export interface Schema {
+    name: string;
+    path: string;
+    stylesheet: boolean;
+    store: boolean;
+    prefix: string;
+}
+
+function loadWorkspace(tree: Tree): any {
+    const workspaceConfig = tree.read("/angular.json");
+    if (!workspaceConfig) return;
+
+    const workspaceContent = workspaceConfig.toString();
+    return JSON.parse(workspaceContent);
+}
+
+function getProject(workspace: any): any {
+    const projectName = workspace.defaultProject;
+    if (!projectName) return;
+
+    return workspace.project?.[projectName];
+}
+
+function configureOptions(options: Schema, tree: Tree): void {
+    const workspace = loadWorkspace(tree);
+    if (!workspace) return;
+
+    const project = getProject(workspace);
+    if (!project) return;
+
+    options.prefix = project.prefix;
+
+    if (options.path === undefined) {
+        // https://github.com/angular/angular-cli/blob/94288c7414b79432c12c21947320a549c1752266/packages/schematics/angular/utility/workspace.ts#L76
+        const root = project.sourceRoot
+            ? `/${project.sourceRoot}/`
+            : `/${project.root}/src/`;
+        const projectDirName =
+            project.extensions["projectType"] === "application" ? "app" : "lib";
+        options.path = `${root}${projectDirName}`;
+    }
+}
 
 export function component(options: Schema): Rule {
     return (tree: Tree, _context: SchematicContext) => {
+        configureOptions(options, tree);
+
         const sourceTemplates = url("./files");
 
         const sourceParametrizedTemplates = apply(sourceTemplates, [
-            filter((path => {
+            filter((path) => {
                 if (path.endsWith(".style.scss") && !options.stylesheet) {
                     return false;
                 }
 
-                if (stringEndsWithAny(path, [".query.ts", ".service.ts", ".store.ts"]) && !options.store) {
+                if (
+                    stringEndsWithAny(path, [
+                        ".query.ts",
+                        ".service.ts",
+                        ".store.ts",
+                    ]) &&
+                    !options.store
+                ) {
                     return false;
                 }
 
                 return true;
-            })),
+            }),
             template({
                 ...options,
                 ...strings,
-                prefix: extractPrefix(tree)
-            })
+            }),
+            move(options.path),
         ]);
 
         return mergeWith(sourceParametrizedTemplates);
     };
-}
-
-function extractPrefix(tree: Tree): string | undefined {
-    const workspaceConfig = tree.read("/angular.json");
-    if (!workspaceConfig) return;
-    
-    const workspaceContent = workspaceConfig.toString();
-    const workspace = JSON.parse(workspaceContent);
-
-    const projectName = workspace.defaultProject;
-    if (!projectName) return;
-
-    const project = workspace.projects[projectName];
-
-    return project?.prefix;
 }
 
 function stringEndsWithAny(source: string, searchStrings: string[]): boolean {
