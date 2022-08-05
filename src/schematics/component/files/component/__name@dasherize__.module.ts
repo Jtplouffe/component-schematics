4eceeec05e01e9179da045c65ed162c9b4a41f4f
Jtplouffe <32107801+Jtplouffe@users.noreply.github.com>
2022-08-05 14:53:17 -0400

    Minor bug fixes

--- src/schematics/component/files/component/__name@dasherize__.module.ts
+++ src/schematics/component/files/component/__name@dasherize__.module.ts
@@ -3,10 +3,12 @@ import { <%= classify(name) %>Component } from "./<%= dasherize(name) %>.compone
 import { <%= classify(name) %>Query } from "./store/<%= dasherize(name) %>.query";
 import { <%= classify(name) %>Service } from "./store/<%= dasherize(name) %>.service";
 import { <%= classify(name) %>Store } from "./store/<%= dasherize(name) %>.store";<% } if (modal) { %>
-import { <%= classify(name) %>Directive } from "./<%= dasherize(name) %>.directive";<% } %>
+import { <%= classify(name) %>Directive } from "./<%= dasherize(name) %>.directive";
+import { MatDialogModule } from "@angular/material/dialog";<% } %>
 
 @NgModule({
-    declarations: [<%= classify(name) %>Component<% if (modal) { %>, <%= classify(name) %>Directive<% } %>],<% if (store) { %>
+    declarations: [<%= classify(name) %>Component<% if (modal) { %>, <%= classify(name) %>Directive<% } %>],
+    imports: [<% if (modal) { %>MatDialogModule<% } %>],<% if (store) { %>
     providers: [<%= classify(name) %>Query, <%= classify(name) %>Service, <%= classify(name) %>Store],<% } %>
     exports: [<%= classify(name) %>Component<% if (modal) { %>, <%= classify(name) %>Directive<% } %>]
 })
--- src/schematics/component/files/modal/__name@dasherize__.directive.ts
+++ src/schematics/component/files/modal/__name@dasherize__.directive.ts
@@ -4,7 +4,7 @@ import { <%= classify(name) %>Component } from "./<%= dasherize(name) %>.compone
 import { <%= classify(name) %>Data } from "./<%= dasherize(name) %>.data";
 import { NiceModalOnClickDirective } from "@recursyve/nice-ui-kit.v2";
 
-@Directive({ selector: "[<%= camelizeWithSuffix((prefix ?? '') + name, 'Modal') %>]" })
+@Directive({ selector: "[<%= camelizeWithSuffix((prefix ?? '') + classify(name), 'Modal') %>]" })
 export class <%= classify(name) %>Directive extends NiceModalOnClickDirective<
     <%= classify(name) %>Component,
     any,
